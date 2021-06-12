9932fb6c9098cfbf01dc215518744a3eb89add7e
Benjamin Roedell <broedell@usbnc.org>
2021-06-12 09:44:43 -0400

    Testing encoding for single quote

---
+++ EncodingTests/VerifyEncoding.cs
@@ -0,0 +1,23 @@
+using System.Text;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Xunit;
+
+namespace EncodingTests
+{
+    public class VerifyEncoding
+    {
+        private const string RightSingleQuoteAkaApostrophe = "’";
+
+        [Fact]
+        public void RightSingleQuoteAkaApostropheTest()
+        {
+            using (new AssertionScope())
+            {
+                Encoding.Default.GetBytes(RightSingleQuoteAkaApostrophe).Should().Equal(0xE2, 0x80, 0x99);
+                Encoding.UTF8.GetBytes(RightSingleQuoteAkaApostrophe).Should().Equal(0xE2, 0x80, 0x99);
+                Encoding.ASCII.GetBytes(RightSingleQuoteAkaApostrophe).Should().Equal(0x3F);
+            }
+        }
+    }
+}
